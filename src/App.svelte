ef2498347c121cdcc7419da702377d9e9dcd8ba6
Victor Perron <victor.perron@polyconseil.fr>
2020-05-10 15:56:00 +0200

    Use a propser Card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,12 @@
 <script>
-  import cardBackground from './assets/img/card-background.png'
   import pigalleCards from './cards'
+  import Card from './Card.svelte'
 
   function shuffledCards () {
-    console.log(pigalleCards)
     return pigalleCards.sort(() => 0.5 - Math.random())
   }
 
-  // This should be a real Svelte Card component
-  function cardComponent (card) {
-    const self = {
-      ...card,
-      src: null,
-      isClicked: false
-    }
-    self.onClick = function () {
-      card.module().then((data)=> {
-        self.src = data.default
-        self.isClicked = true
-        items = items
-      })
-    }
-    return self
-  }
-
-  const items = shuffledCards().slice(0, 3).map((card) => cardComponent(card))
+  const items = shuffledCards().slice(0, 3)
 </script>
 
 <wrapper>
@@ -34,10 +16,7 @@
     <row>
       {#each items as item}
         <column>
-          <card>
-            <img alt={item.name} src={item.src} class="{item.isClicked ? 'visible' : 'hidden'}" />
-            <img alt='card background' src={cardBackground} class="{item.isClicked ? 'hidden' : 'visible'}" on:click={item.onClick}/>
-          </card>
+          <Card name={item.name} imageModule={item.module} />
         </column>
       {/each}
     </row>
@@ -74,45 +53,10 @@ column {
   align-items: center;
 }
 
-card {
-  height: 400px;
-  position: relative;
-}
-
-card img {
-  cursor: pointer;
-  position: absolute;
-  /* because card is about 230px in size */
-  left: -115px;
-  height: 100%;
-  border-radius: 12px;
-  box-shadow: 0px 0px 10px #ddd;
-  /* box-shadow: 0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12); */
-
-  transition: all 1.5s ease-in-out;
-}
-
-/*
-@media min-width: 601px {
-}
-*/
-
 @media (max-width: 600px) {
   row {
     flex-direction: column;
   }
 }
 
-.hidden {
-  opacity: 0;
-  transform: rotateY(180deg);
-  transform-origin: 50% 0%;
-
-}
-
-.visible {
-  opacity: 1;
-  transform: rotateY(0deg);
-  transform-origin: 50% 0%;
-}
 </style>
---
+++ src/Card.svelte
@@ -0,0 +1,54 @@
+<script>
+import cardBackground from './assets/img/card-background.png'
+
+let src = null
+let isClicked = false
+
+export let name;
+export let imageModule;
+
+function onClick () {
+  imageModule().then((data)=> {
+    src = data.default
+    isClicked = true
+  })
+}
+</script>
+
+<card>
+  <img alt={name} src={src} class="{isClicked ? 'visible' : 'hidden'}" />
+  <img alt='card background' src={cardBackground} class="{isClicked ? 'hidden' : 'visible'}" on:click={onClick}/>
+</card>
+
+<style>
+card {
+  height: 400px;
+  position: relative;
+}
+
+img {
+  cursor: pointer;
+  position: absolute;
+  /* because card is about 230px in size */
+  left: -115px;
+  height: 100%;
+  border-radius: 12px;
+  box-shadow: 0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12);
+
+  transition: all 1.5s ease-in-out;
+}
+
+
+.hidden {
+  opacity: 0;
+  transform: rotateY(180deg);
+  transform-origin: 50% 0%;
+
+}
+
+.visible {
+  opacity: 1;
+  transform: rotateY(0deg);
+  transform-origin: 50% 0%;
+}
+</style>
