434d8cf84f39833dd61c5ae289f2f2a4cf55d3ea
Victor Perron <victor.perron@polyconseil.fr>
2020-05-10 15:55:39 +0200

    Replace background by a gradient

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import './global.css'
 
-// This is a shame, there is currently no rollup plugin that would interpret and
-// create hashed URL for url() inside CSS files. We need to do it ourselves :(
-import bodyBg from './assets/img/bg.jpg'
-document.body.style.background = `url('${bodyBg}') no-repeat center center fixed`
-document.body.style['background-size'] = 'cover'
-
 import App from './App.svelte'
 
 const app = new App({
